58ed83d8c09eecc622ab6ff11ea24f3fc26c9092
lalitha-gith <130900867+lalitha-gith@users.noreply.github.com>
2025-04-26 20:23:02 +0530

    Add files via upload

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,24 @@
+const mongoose = require( "mongoose");
+
+const Userschema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    number:{
+        type:Number,
+        unique:true
+    },
+    score:{
+        type:Number,
+        default:0
+    }
+})
+
+module.exports = mongoose.model("User", Userschema);
 
